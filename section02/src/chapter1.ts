d595ba23987752d1b2aafcdf375681e6123f5ccb
이범진 <lbj0797@dataeum.kr>
2024-12-02 10:59:31 +0900

    원시타입, 리터럴타입

---
+++ section02/src/chapter1.ts
@@ -0,0 +1,23 @@
+// 원시 타입
+
+// 1. number: 숫자, 소숫점, Infinity, Nan 등
+let num1: number = 123;
+
+// 2. string
+let str1: string = "hello";
+
+// 3. boolean
+let bool1: boolean = true;
+
+// 4. null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// 리터럴 타입, 변수의 타입을 값 그 자체로 정의
+let numA: 10 = 10;
+// let numA: 10 = 12; -> 에러
+let strA: "hello" = "hello";
+let boolA: true = true;
+// let boolA: true = false; -> 에러
